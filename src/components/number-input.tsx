d3269e48477257104c4b71bc5111dfe0ecca6c67
autopp <autopp.inc@gmail.com>
2021-04-23 09:53:29 +0900

    feat: add NumberInput

---
+++ src/components/number-input.tsx
@@ -0,0 +1,33 @@
+import { FC } from "react"
+
+type Props = {
+  state: [number, (value: number) => void]
+  min: number
+  max: number
+  step: number
+}
+
+const NumberInput: FC<Props> = ({ state: [value, onChange], min, max, step }: Props) => {
+  const parseValue = (v: string) => {
+    const n = parseInt(v, 10)
+    if (n < min) {
+      return min
+    }
+    if (n > max) {
+      return max
+    }
+    return n
+  }
+  return (
+    <input
+      type="number"
+      value={value}
+      min={min}
+      max={max}
+      step={step}
+      onChange={(e) => onChange(parseValue(e.target.value))}
+    />
+  )
+}
+
+export default NumberInput
