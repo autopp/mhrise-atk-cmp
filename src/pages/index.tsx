e1ab768902667f21db415b255acac77df2835b59
autopp <autopp.inc@gmail.com>
2021-04-26 09:51:12 +0900

    feat: show weapon attack

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from "next/head"
-import { FC } from "react"
+import { FC, useState } from "react"
 import Layout, { siteTitle } from "@/components/layout"
+import HeadingRow from "@/components/heading-row"
+import NumberInputRow from "@/components/number-input-row"
 
 const Home: FC = () => {
+  const leftWeaponAttack = useState(180)
+  const rightWeaponAttack = useState(180)
   return (
     <Layout>
       <Head>
@@ -14,6 +18,17 @@ const Home: FC = () => {
             <h1>{siteTitle}</h1>
           </div>
         </div>
+        <HeadingRow text="武器" />
+        <NumberInputRow
+          label="武器攻撃力"
+          min={0}
+          max={300}
+          step={10}
+          synced={false}
+          setSync={() => undefined}
+          left={leftWeaponAttack}
+          right={rightWeaponAttack}
+        />
       </div>
     </Layout>
   )
